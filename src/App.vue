770788b63e0bc41666f690d35f559b4550dcea27
semiaddict <info@semiaddict.com>
2021-10-06 10:30:52 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <transition name="fade">
+      <i18n-t keypath="hello" tag="p">
+        <template #world>
+            <b>{{ $t("world") }}</b>
+        </template>
+      </i18n-t>
+  </transition>
+</template>
+
+<script>
+export default {
+  i18n: {
+    messages: {
+      en: {
+        hello: "Hello {world}",
+        world: "world!",
+      },
+    },
+  }
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import { createI18n } from "vue-i18n";
+import App from './App.vue'
+
+const i18n = createI18n({ locale: "en" });
+
+createApp(App).use(i18n).mount('#app')
