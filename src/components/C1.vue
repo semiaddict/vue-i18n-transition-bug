e50c700c994c11af146db4277ca36992eff63a07
semiaddict <info@semiaddict.com>
2021-10-07 11:41:20 +0200

    add descendent component with i18n messages

--- src/components/C1.vue
+++ src/components/C1.vue
@@ -17,15 +17,27 @@
           <strong>{{ $t("world") }}</strong>
         </template>
       </i18n-t>
+
+      <C3>
+      <div>{{ $t("hello", { world: $t("world") }) }}</div>
+      <i18n-t keypath="hello" tag="div">
+        <template #world>
+          <strong>{{ $t("world") }}</strong>
+        </template>
+      </i18n-t>
+      </C3>
     </C2>
   </div>
 </template>
 
 <script>
 import C2 from "./C2";
+import C3 from "./C3";
+
 export default {
   components: {
     C2,
+    C3,
   },
   i18n: {
     messages: {
---
+++ src/components/C3.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>C3 slot: <slot></slot></div>
+</template>
+
+<script>
+export default {
+  i18n: {
+    messages: {
+      en: {
+        hello: "Hello {world} - C3",
+        world: "world! - C3",
+      },
+    },
+  },
+};
+</script>
